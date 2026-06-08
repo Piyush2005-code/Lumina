f234f707ca5e94b362d8f46d561592e5ce2cfa02
Piyush2005-code <piyush.bhati680gmail.com>
2026-06-08 09:43:46 +0530

    Adding the shell mcp server

---
+++ backend/servers/shellMCP/server.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+
+# result = subprocess.run("echo Hello world I am Piyush Singh Bhati", shell = True, capture_output=True, text=True)
+
+# print(result.stdout.strip())
+
+os.system("cat /Users/piyushsinghbhati/Documents/Programming/agents-mcp/Lumina.tex")
